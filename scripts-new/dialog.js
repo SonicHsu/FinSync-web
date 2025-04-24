bc28d63956c5cb267aef364cf16f80be8799e2d0
unknown <soniccx0814@cts.com.tw>
2025-04-24 16:27:15 +0800

    dialog 製作中

--- scripts-new/dialog.js
+++ scripts-new/dialog.js
@@ -1,12 +1,23 @@
 export function initDialog(name) {
     const dialogElement = document.querySelector(`[data-dialog="${name}"]`);
+    const dialogBackDrop = document.querySelector("[data-dialog-backdrop]");
+
+
+    dialogElement.addEventListener("click", (event) => {
+        if (event.target === dialogElement) {
+            dialogElement.close();
+            dialogBackDrop.classList.add("hidden");
+        }
+    })
 
     return {
         open() {
             dialogElement.showModal();
+            dialogBackDrop.classList.remove("hidden");
         },
         close() {
             dialogElement.close();
+            dialogBackDrop.classList.add("hidden");
         }
     };
 }
